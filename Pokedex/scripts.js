485bc22f35ad22cdb0c77cbda2744f9f0b3e77b5
rafael rocha <1rafaelrocha2@gmail.com>
2022-03-14 10:04:59 -0300

    update-grama

---
+++ Pokedex/scripts.js
@@ -0,0 +1,21 @@
+const listaSelecaoPokemon = document.querySelectorAll('.pokemon');
+const pokemonCard = document.querySelectorAll('.cartao-pokemon');
+
+listaSelecaoPokemon.forEach(pokemon => {
+    pokemon.addEventListener('click', () => {
+        const cartaoPokemonAberto = document.querySelector('.aberto')
+        cartaoPokemonAberto.classList.remove('aberto')
+
+        const idPokemonSelecionado = pokemon.attributes.id.value
+
+        const cartaoPokemonParaAbrir = document.getElementById('cartao-' + idPokemonSelecionado)
+        cartaoPokemonParaAbrir.classList.add('aberto')
+
+        const pokemonAbertoNaListagem = document.querySelector('.ativo')
+        pokemonAbertoNaListagem.classList.remove('ativo')
+
+        const pokemonSelecionadoNaListagem = document.getElementById(idPokemonSelecionado)
+        pokemonSelecionadoNaListagem.classList.add('ativo')
+
+    })
+})
