e2ad660c3888ce83a633262fb9a2346841ea9d48
rafael rocha <1rafaelrocha2@gmail.com>
2022-03-19 11:29:45 -0300

    update

---
+++ PixelArt/script.js
@@ -0,0 +1,6 @@
+// Começo do Projeto! 
+function criarElementos() {
+    const main = document.querySelectorAll('.main-container');
+    const divs = document.createElement('div');
+    main.appendChild(divs);
+}
