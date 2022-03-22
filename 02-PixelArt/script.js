ad5869800672aed8f5b0ac80c7800793f2e6e0fb
rafael rocha <1rafaelrocha2@gmail.com>
2022-03-21 21:11:08 -0300

    update

--- 02-PixelArt/script.js
+++ 02-PixelArt/script.js
@@ -1,42 +1,87 @@
-// Exercício 02 (01 e 03 foram feitos via html e css)
 function colorFunction() {
-    const gerarCor = document.querySelector('#color-palette');
-    const listaCores = ['','','',]
-    for (let i = 0; i < listaCores.length; i+=1) {
-        const listaCor = listaCores[i];
+  const gerarCor = document.querySelector('#color-palette');
+  const listaCores = ['', '', ''];
+  for (let i = 0; i < listaCores.length; i += 1) {
+    const listaCor = listaCores[i];
 
-        const divColor = document.createElement('div');
-        
-        divColor.style.backgroundColor = gerar_cor_hexadecimal();
+    const divColor = document.createElement('div');
 
-        divColor.classList.add('color');
-        divColor.innerHTML = listaCor;
-        gerarCor.appendChild(divColor);    
-    }
+    divColor.style.backgroundColor = gerar_cor_hexadecimal();
 
-function gerar_cor_hexadecimal(){
+    divColor.classList.add('color');
+    divColor.innerHTML = listaCor;
+    gerarCor.appendChild(divColor);
+  }
+
+  function gerar_cor_hexadecimal() {
     //Vi nesse site como gerar uma cor aleatória: https://wallacemaxters.com.br/blog/2021/02/20/como-gerar-cores-aleatorias-no-javascript
-  return '#' + parseInt((Math.random() * 0xFFF))
-    .toString(16)
-    .padStart(3, '0');
+    return (
+      '#' +
+      parseInt(Math.random() * 0xfff)
+        .toString(16)
+        .padStart(3, '0')
+    );
+  }
 }
-};
 colorFunction();
 
-
 function gerarPixels() {
-  const tamanho = 5;
-
-  for (let quadroLat = 0; quadroLat < tamanho; quadroLat += 1) {
+  const tamanho = 9;
+  for (let boardQuadro = 0; boardQuadro < tamanho; boardQuadro += 1) {
     const divQuadro = document.querySelector('#pixel-board');
     const divLinha = document.createElement('div');
     divQuadro.appendChild(divLinha);
 
-    for (let p = 0; p < tamanho; p += 1) {
+    for (let boardLinhas = 0; boardLinhas < tamanho; boardLinhas += 1) {
       const pixels = document.createElement('div');
       pixels.classList.add('pixel');
       divQuadro.appendChild(pixels);
     }
   }
-};
+}
 gerarPixels();
+
+function colorSelected() {
+  const mycolors = document.querySelectorAll('.color');
+
+  for (let index = 0; index < mycolors.length; index += 1) {
+    const element = mycolors[index];
+
+    element.addEventListener('click', (event) => {
+      const removerSelecionado = document.querySelectorAll('.selected');
+
+      for (let rmv = 0; rmv < removerSelecionado.length; rmv += 1) {
+        removerSelecionado[rmv].classList.remove('selected');
+      }
+      event.target.classList.add('selected');
+    });
+  }
+}
+colorSelected();
+
+function pintarGrid() {
+  //Fiz esse exercício com a ajuda da colega giovanna morais, links que usamos de referencia:
+  //https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle
+  //https://developer.mozilla.org/en-US/docs/Web/API/CSSStyleDeclaration/getPropertyValue
+  const pixelGrid = document.getElementsByClassName('pixel');
+  for (let pixelColor = 0; pixelColor < pixelGrid.length; pixelColor += 1) {
+    pixelGrid[pixelColor].addEventListener('click', () => {
+      const corSelecionada = document.querySelector('.selected');
+      const corSelecionadacss = window.getComputedStyle(corSelecionada);
+      const cssEstilo = corSelecionadacss.getPropertyValue('background-color');
+      pixelGrid[pixelColor].style.backgroundColor = cssEstilo;
+    });
+  }
+}
+pintarGrid();
+
+function clear() {
+  const pixelsClear = document.getElementsByClassName('pixel');
+  const clearAll = document.getElementById('clear-board');
+  clearAll.addEventListener('click', () => {
+    for (let z = 0; z < pixelsClear.length; z += 1) {
+      pixelsClear[z].style.backgroundColor = 'white';
+    }
+  });
+}
+clear();
