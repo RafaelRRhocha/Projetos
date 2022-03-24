8e68c78551982c85dbfa2a6fa3af8f5f274c83ca
rafael rocha <1rafaelrocha2@gmail.com>
2022-03-23 21:23:14 -0300

    update

--- 03-listaDeTarefas/script.js
+++ 03-listaDeTarefas/script.js
@@ -3,6 +3,12 @@ const enterButton = document.getElementById('criar-tarefa');
 const enterOl = document.getElementById('lista-tarefas');
 const btnClear = document.getElementById('apaga-tudo');
 const btnClearFinish = document.getElementById('remover-finalizados');
+const btnClearSelected = document.getElementById('remover-selecionado');
+const btnSalvar = document.getElementById('salvar-tarefas');
+const btnCima = document.getElementById('mover-cima');
+const btnBaixo = document.getElementById('mover-baixo');
+
+//Referencia Arrow Functions: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Functions/Arrow_functions
 
 enterButton.addEventListener('click', () => {
   if (inputLength() > 0) {
@@ -22,13 +28,43 @@ btnClear.addEventListener('click', () => {
 });
 
 btnClearFinish.addEventListener('click', () => {
-  const pegarSelecionado = document.getElementsByClassName('completed');
-  for (let c = 0; c < pegarSelecionado.length; c+=1) {
-    pegarSelecionado[c].remove();
-    
+  const pegarFinalizado = document.querySelectorAll('.completed');
+  for (let c = 0; c < pegarFinalizado.length; c+=1) {
+    pegarFinalizado[c].remove();
   }
-
 });
+
+btnClearSelected.addEventListener('click', () => {
+  const pegarSelecionado = document.getElementsByClassName('selected');
+  for (let f = 0; f < pegarSelecionado.length; f+=1) {
+    pegarSelecionado[f].remove();
+  }
+});
+
+btnSalvar.addEventListener('click', () => {
+  localStorage.setItem('list', enterOl.innerHTML)
+});
+
+btnCima.addEventListener('click', () => {
+  //fiz esse exercício com a ajuda dos colegas adalberto e ciro!
+  //link de referencia do insertBefore: https://developer.mozilla.org/pt-BR/docs/Web/API/Node/insertBefore
+  const selecionarCima = document.querySelector('.selected');
+  if (selecionarCima !== enterOl.firstElementChild && selecionarCima) {
+    enterOl.insertBefore(selecionarCima, selecionarCima.previousSibling);
+  }
+  console.log(selecionarCima);
+})
+
+btnBaixo.addEventListener('click', () => {
+  const selecionarBaixo = document.querySelector('.selected');
+  if (selecionarBaixo !== enterOl.lastElementChild && selecionarBaixo) {
+    enterOl.insertBefore(selecionarBaixo, selecionarBaixo.nextSibling.nextSibling);
+  }
+})
+
+window.onload = () => {
+  enterOl.innerHTML = localStorage.getItem('list');
+};
 
 function inputLength() {
   return enterInput.value.length;
@@ -44,15 +80,16 @@ function criarTarefas() {
   enterInput.value = '';
 
   criarLi.addEventListener('click', (event) => {
-    const removerSelecionado = document.querySelectorAll('.bgColor');
+    const removerSelecionado = document.querySelectorAll('.selected');
 
     for (let rmv = 0; rmv < removerSelecionado.length; rmv += 1) {
-      removerSelecionado[rmv].classList.remove('bgColor');
+      removerSelecionado[rmv].classList.remove('selected');
     }
-    event.target.classList.add('bgColor');
+    event.target.classList.add('selected');
   });
 
   criarLi.addEventListener('dblclick', () => {
+    //Referencia toggle: https://developer.mozilla.org/en-US/docs/Web/API/DOMTokenList/toggle
     criarLi.classList.toggle('completed');
   });
 }
