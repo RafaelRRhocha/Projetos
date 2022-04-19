06b227a0b3528bc91b613a1e3cb22600d48c1997
rafael rocha <1rafaelrocha2@gmail.com>
2022-04-19 20:46:35 -0300

    update js

--- Pokedex/scripts.js
+++ Pokedex/scripts.js
@@ -19,3 +19,33 @@ listaSelecaoPokemon.forEach(pokemon => {
 
     })
 })
+
+const getPokemonUrl = (id) => `https://pokeapi.co/api/v2/pokemon/${id}`;
+const buscarPokemon = () => {
+
+  const pokemonPromisses = [];
+
+  for (let index = 1; index <= 151; index++) {
+    pokemonPromisses.push(fetch(getPokemonUrl(index)).then(response => response.json()))
+  }
+
+  Promise.all(pokemonPromisses) 
+    .then(pokemons => {
+      const liPoke = pokemons.reduce((acc, pokes) => {
+
+        const types = pokes.types.map(typeInfo => typeInfo.type.name);
+
+        acc += `
+          <li class="card ${types[0]}">
+            <img class="card-image" alt="${pokes.name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokes.id}.svg"/>
+            <h2 class="card-title">#${pokes.id} - ${pokes.name}</h2>
+            <p class="card-subtitle">${types.join(' | ')}</p>
+          </li>`
+        return acc;
+      }, '')
+
+      const ul = document.querySelector('[data-js="pokedex"]');
+      ul.innerHTML = liPoke;
+    })
+}
+buscarPokemon();
