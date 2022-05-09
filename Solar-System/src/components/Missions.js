c1b281ce08a0b9a4cf6057228a8205dde8a5c501
rafael rocha <1rafaelrocha2@gmail.com>
2022-05-09 17:14:52 -0300

    push solar system

---
+++ Solar-System/src/components/Missions.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import MissionCard from './MissionCard';
+import Title from './Title';
+import missions from '../data/missions';
+
+class Missions extends React.Component {
+  render() {
+    return (
+      <div data-testid="missions">
+        <Title headline="Missões" />
+        {missions.map((mission) => (<MissionCard
+          key={ mission.name }
+          name={ mission.name }
+          year={ mission.image }
+          country={ mission.image }
+          destination={ mission.image }
+        />))}
+      </div>
+    );
+  }
+}
+
+export default Missions;
