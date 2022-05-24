7532fffec57124ed697d730e8be04bb708dfbcd9
rafael rocha <1rafaelrocha2@gmail.com>
2022-05-24 12:00:55 -0300

    update 404

--- trybeTunes/src/pages/NotFound.jsx
+++ trybeTunes/src/pages/NotFound.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React from "react";
+import "../Css/PageNotFound.css";
 
 export default class NotFound extends React.Component {
   render() {
     return (
-      <div data-testid="page-not-found" />
+      <div className="div-error" >
+        <div className="error" title="404">404</div> 
+      </div>
     );
   }
 }
